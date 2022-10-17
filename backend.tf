65c107d25d5b4afa577832fc631f36c83ce57afa
SeungwooLee <kallusis369@gmail.com>
2022-10-17 17:58:32 +0900

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,13 +1,13 @@
 # Terraform docs re: configuring back end: https://www.terraform.io/docs/backends/types/gcs.html
+#terraform {
+#  backend "gcs" {
+#    prefix = "terraform/state"
+#    bucket = "bkt-prod-clouddevops-sandbox-terraform-state" # Terraform state를 저장할 GCS 버켓 이름을 기입
+#  }
+#}
+
 terraform {
-  backend "gcs" {
-    prefix = "terraform/state"
-    bucket = "bkt-prod-clouddevops-sandbox-terraform-state" # Terraform state를 저장할 GCS 버켓 이름을 기입
+  backend "local" {
   }
 }
 
-#terraform {
-#  backend "local" {
-#  }
-#}
-#
