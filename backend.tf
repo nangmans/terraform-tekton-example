0c07a297ac3f7662a2faca9d1ac7a592f8caf9c2
SeungwooLee <kallusis369@gmail.com>
2022-10-17 17:05:03 +0900

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 # Terraform docs re: configuring back end: https://www.terraform.io/docs/backends/types/gcs.html
 terraform {
   backend "gcs" {
-    #prefix = "terraform/state"
+    prefix = "terraform/state"
     bucket = "bkt-prod-clouddevops-sandbox-terraform-state" # Terraform state를 저장할 GCS 버켓 이름을 기입
-    impersonate_service_account = "sa-dev-terraform-test@prj-sandbox-devops-9999.iam.gserviceaccount.com"
   }
 }
 
